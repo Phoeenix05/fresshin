180fa90bec0f43e229f79b31ef0d537e128dd19c
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-07-13 21:57:51 +0300

    Added character page

---
+++ routes/character/[name].tsx
@@ -0,0 +1,25 @@
+import {Handlers, PageProps } from "$fresh/server.ts"
+
+export const handler: Handlers<string | null> = {
+    async GET(_, ctx) {
+        const { name } = ctx.params
+        const resp = await fetch(`https://api.genshin.dev/characters/${name}`)
+        if (resp.status == 404) {
+            return ctx.render(null)
+        }
+        const character = await resp.json()
+        return ctx.render(character)
+    }
+}
+
+export default function CharacterPage({ data }: PageProps<string | null>) {
+    if (!data) {
+        return <h1>Character doesn't exist</h1>
+    }
+
+    return (
+        <main>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </main>
+    )
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -7,19 +7,10 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Fresshin - Genshin Impact</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh.
-        </p>
-        <Counter count={count} />
+        Search characters by changing the url to `/character/[character name]`
       </div>
     </>
   );
